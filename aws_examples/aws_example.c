176c3f7a1492ec6a5cca12017c437cca287e17c8
Ravishankar Bhagavandas <bhagavar@amazon.com>
2021-11-16 09:59:13 -0800

    Add tcp/ip (lwip) init

--- aws_examples/aws_example.c
+++ aws_examples/aws_example.c
@@ -40,168 +40,201 @@ to be available here. */
 #include "clock_config.h"
 #include "board.h"
 
+
+#include "lwip/dhcp.h"
+#include "lwip/ip_addr.h"
+#include "lwip/netifapi.h"
+#include "lwip/prot/dhcp.h"
+#include "lwip/tcpip.h"
+#include "lwip/sys.h"
+
+#include "fsl_phy.h"
+#include "enet_ethernetif.h"
+#include "fsl_phyksz8081.h"
+#include "fsl_enet_mdio.h"
+#include "fsl_iomuxc.h"
+
+/******************************************
+ * Ethernet Configuration.
+ ******************************************/
+
+/* IP address configuration. */
+#define configIP_ADDR0 192
+#define configIP_ADDR1 168
+#define configIP_ADDR2 0
+#define configIP_ADDR3 102
+
+/* Netmask configuration. */
+#define configNET_MASK0 255
+#define configNET_MASK1 255
+#define configNET_MASK2 255
+#define configNET_MASK3 0
+
+/* Gateway address configuration. */
+#define configGW_ADDR0 192
+#define configGW_ADDR1 168
+#define configGW_ADDR2 0
+#define configGW_ADDR3 100
+
+/* ENET clock frequency. */
+#define EXAMPLE_CLOCK_FREQ CLOCK_GetFreq(kCLOCK_IpgClk)
+#ifndef EXAMPLE_NETIF_INIT_FN
+/*! @brief Network interface initialization function. */
+#define EXAMPLE_NETIF_INIT_FN ethernetif0_init
+#endif /* EXAMPLE_NETIF_INIT_FN */
+
+
+/*! @brief Stack size of the temporary lwIP initialization thread. */
+#define INIT_THREAD_STACKSIZE 1024
+
+/*! @brief Priority of the temporary lwIP initialization thread. */
+#define INIT_THREAD_PRIO DEFAULT_THREAD_PRIO
+
+/* MDIO operations. */
+#define EXAMPLE_MDIO_OPS enet_ops
+
+/* PHY operations. */
+#define EXAMPLE_PHY_OPS phyksz8081_ops
+
+/* MAC address configuration. */
+#define configMAC_ADDR                     \
+    {                                      \
+        0x04, 0x12, 0x13, 0xB1, 0x11, 0x90 \
+    }
+
+/* Address of PHY interface. */
+#define EXAMPLE_PHY_ADDRESS ((uint32_t)BOARD_ENET0_PHY_ADDRESS)
+
+/* System clock name. */
+#define EXAMPLE_CLOCK_NAME kCLOCK_CoreSysClk
+
+/* Facilitate a simple hash for unique MAC Address based on an input 18 byte UID */
+#define MAC_HASH(N)  \
+    fsl_enet_config0.macAddress[N] = buffer[(N+2)+(5*0)] ^ buffer[(N+2)+(5*1)] ^ buffer[(N+2)+(5*2)]
+
 /*******************************************************************************
- * Definitions
+ * Static variables
  ******************************************************************************/
+static struct netif fsl_netif0;
+static mdio_handle_t mdioHandle = {.ops = &EXAMPLE_MDIO_OPS};
+static phy_handle_t phyHandle   = {.phyAddr = EXAMPLE_PHY_ADDRESS, .mdioHandle = &mdioHandle, .ops = &EXAMPLE_PHY_OPS};
 
-/* Priorities at which the tasks are created.  The event semaphore task is
-given the maximum priority of ( configMAX_PRIORITIES - 1 ) to ensure it runs as
-soon as the semaphore is given. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY   (tskIDLE_PRIORITY + 2)
-#define mainQUEUE_SEND_TASK_PRIORITY      (tskIDLE_PRIORITY + 1)
-#define mainEVENT_SEMAPHORE_TASK_PRIORITY (configMAX_PRIORITIES - 1)
-
-/* The rate at which data is sent to the queue, specified in milliseconds, and
-converted to ticks using the portTICK_PERIOD_MS constant. */
-#define mainQUEUE_SEND_PERIOD_MS (200 / portTICK_PERIOD_MS)
-
-/* The period of the example software timer, specified in milliseconds, and
-converted to ticks using the portTICK_PERIOD_MS constant. */
-#define mainSOFTWARE_TIMER_PERIOD_MS (1000 / portTICK_PERIOD_MS)
-
-/* The number of items the queue can hold.  This is 1 as the receive task
-will remove items as they are added, meaning the send task should always find
-the queue empty. */
-#define mainQUEUE_LENGTH (1)
-
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
-/*
- * The queue send and receive tasks as described in the comments at the top of
- * this file.
- */
-static void prvQueueReceiveTask(void *pvParameters);
-static void prvQueueSendTask(void *pvParameters);
-
-/*
- * The callback function assigned to the example software timer as described at
- * the top of this file.
- */
-static void vExampleTimerCallback(TimerHandle_t xTimer);
-
-/*
- * The event semaphore task as described at the top of this file.
- */
-static void prvEventSemaphoreTask(void *pvParameters);
-
-/*******************************************************************************
- * Globals
- ******************************************************************************/
-/* The queue used by the queue send and queue receive tasks. */
-static QueueHandle_t xQueue = NULL;
-
-/* The semaphore (in this case binary) that is used by the FreeRTOS tick hook
- * function and the event semaphore task.
- */
-static SemaphoreHandle_t xEventSemaphore = NULL;
-
-/* The counters used by the various examples.  The usage is described in the
- * comments at the top of this file.
- */
-static volatile uint32_t ulCountOfTimerCallbackExecutions = 0;
-static volatile uint32_t ulCountOfItemsReceivedOnQueue    = 0;
-static volatile uint32_t ulCountOfReceivedSemaphores      = 0;
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
+
+void BOARD_InitModuleClock(void)
+{
+    const clock_enet_pll_config_t config = {.enableClkOutput = true, .enableClkOutput25M = false, .loopDivider = 1};
+    CLOCK_InitEnetPll(&config);
+}
+/*!
+ * @brief Initializes lwIP stack.
+ *
+ * @param arg unused
+ */
+static void stack_init(void *arg)
+{
+    static struct netif netif;
+    ip4_addr_t netif_ipaddr, netif_netmask, netif_gw;
+    ethernetif_config_t enet_config = {
+        .phyHandle  = &phyHandle,
+        .macAddress = configMAC_ADDR,
+    };
+    struct dhcp *dhcp;
+    u8_t dhcp_last_state = DHCP_STATE_OFF;
+
+    LWIP_UNUSED_ARG(arg);
+
+    mdioHandle.resource.csrClock_Hz = EXAMPLE_CLOCK_FREQ;
+
+    IP4_ADDR(&netif_ipaddr, 0U, 0U, 0U, 0U);
+    IP4_ADDR(&netif_netmask, 0U, 0U, 0U, 0U);
+    IP4_ADDR(&netif_gw, 0U, 0U, 0U, 0U);
+
+    PRINTF("\r\nInitializing LWIP network stack..\r\n");
+
+    tcpip_init(NULL, NULL);
+
+    netif_add(&netif, &netif_ipaddr, &netif_netmask, &netif_gw, &enet_config, EXAMPLE_NETIF_INIT_FN,
+                       tcpip_input);
+    netif_set_default(&netif);
+    netif_set_up(&netif);
+
+    PRINTF("\r\nStarting DHCP negotiation..\r\n");
+
+    dhcp_start(&netif);
+
+    while (netif_is_up(&netif))
+    {
+    	dhcp = netif_dhcp_data(&netif);
+
+    	if (dhcp == NULL)
+    	{
+    		dhcp_last_state = DHCP_STATE_OFF;
+    	}
+    	else if (dhcp_last_state != dhcp->state)
+    	{
+    		dhcp_last_state = dhcp->state;
+    	}
+
+    	PRINTF("\r\nCurrent DHCP state %u\r\n", dhcp_last_state );
+
+
+        if (dhcp_last_state == DHCP_STATE_BOUND)
+        {
+            PRINTF("\r\n IPv4 Address     : %s\r\n", ipaddr_ntoa(&(netif.ip_addr)));
+            PRINTF(" IPv4 Subnet mask : %s\r\n", ipaddr_ntoa(&(netif.netmask)));
+            PRINTF(" IPv4 Gateway     : %s\r\n\r\n", ipaddr_ntoa(&(netif.gw)));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(20));
+
+    }
+
+
+    vTaskDelete(NULL);
+}
+
+void delay(void)
+{
+    volatile uint32_t i = 0;
+    for (i = 0; i < 1000000; ++i)
+    {
+        __asm("NOP"); /* delay */
+    }
+}
+
+
 /*!
  * @brief Main function
  */
 int main(void)
 {
-    TimerHandle_t xExampleSoftwareTimer = NULL;
+	gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
 
     /* Init board hardware. */
     BOARD_ConfigMPU();
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
+    BOARD_InitModuleClock();
 
-    /* Create the queue used by the queue send and queue receive tasks. */
-    xQueue = xQueueCreate(/* The number of items the queue can hold. */
-                          mainQUEUE_LENGTH,
-                          /* The size of each item the queue holds. */
-                          sizeof(uint32_t));
 
-    /* Enable queue view in MCUX IDE FreeRTOS TAD plugin. */
-    if (xQueue != NULL)
-    {
-        vQueueAddToRegistry(xQueue, "xQueue");
-    }
+    IOMUXC_EnableMode(IOMUXC_GPR, kIOMUXC_GPR_ENET1TxClkOutputDir, true);
 
-    /* Create the semaphore used by the FreeRTOS tick hook function and the
-    event semaphore task. */
-    vSemaphoreCreateBinary(xEventSemaphore);
+     GPIO_PinInit(GPIO1, 9, &gpio_config);
+     GPIO_PinInit(GPIO1, 10, &gpio_config);
+     /* pull up the ENET_INT before RESET. */
+     GPIO_WritePinOutput(GPIO1, 10, 1);
+     GPIO_WritePinOutput(GPIO1, 9, 0);
+     delay();
+     GPIO_WritePinOutput(GPIO1, 9, 1);
 
-    /* Create the queue receive task as described in the comments at the top
-    of this    file. */
-    if (xTaskCreate(/* The function that implements the task. */
-                    prvQueueReceiveTask,
-                    /* Text name for the task, just to help debugging. */
-                    "Rx",
-                    /* The size (in words) of the stack that should be created
-                    for the task. */
-                    configMINIMAL_STACK_SIZE + 166,
-                    /* A parameter that can be passed into the task.  Not used
-                    in this simple demo. */
-                    NULL,
-                    /* The priority to assign to the task.  tskIDLE_PRIORITY
-                    (which is 0) is the lowest priority.  configMAX_PRIORITIES - 1
-                    is the highest priority. */
-                    mainQUEUE_RECEIVE_TASK_PRIORITY,
-                    /* Used to obtain a handle to the created task.  Not used in
-                    this simple demo, so set to NULL. */
-                    NULL) != pdPASS)
-    {
-        PRINTF("Task creation failed!.\r\n");
-        while (1)
-            ;
-    }
 
-    /* Create the queue send task in exactly the same way.  Again, this is
-    described in the comments at the top of the file. */
-    if (xTaskCreate(prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE + 166, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL) !=
-        pdPASS)
-    {
-        PRINTF("Task creation failed!.\r\n");
-        while (1)
-            ;
-    }
+    xTaskCreate( stack_init, "LWIP-INIT", INIT_THREAD_STACKSIZE, NULL, INIT_THREAD_PRIO, NULL );
 
-    /* Create the task that is synchronised with an interrupt using the
-    xEventSemaphore semaphore. */
-    if (xTaskCreate(prvEventSemaphoreTask, "Sem", configMINIMAL_STACK_SIZE + 166, NULL,
-                    mainEVENT_SEMAPHORE_TASK_PRIORITY, NULL) != pdPASS)
-    {
-        PRINTF("Task creation failed!.\r\n");
-        while (1)
-            ;
-    }
-
-    /* Create the software timer as described in the comments at the top of
-    this file. */
-    xExampleSoftwareTimer = xTimerCreate(/* A text name, purely to help
-                                       debugging. */
-                                         "LEDTimer",
-                                         /* The timer period, in this case
-                                         1000ms (1s). */
-                                         mainSOFTWARE_TIMER_PERIOD_MS,
-                                         /* This is a periodic timer, so
-                                         xAutoReload is set to pdTRUE. */
-                                         pdTRUE,
-                                         /* The ID is not used, so can be set
-                                         to anything. */
-                                         (void *)0,
-                                         /* The callback function that switches
-                                         the LED off. */
-                                         vExampleTimerCallback);
-
-    /* Start the created timer.  A block time of zero is used as the timer
-    command queue cannot possibly be full here (this is the first timer to
-    be created, and it is not yet running). */
-    xTimerStart(xExampleSoftwareTimer, 0);
 
     /* Start the tasks and timer running. */
     vTaskStartScheduler();
@@ -213,127 +246,6 @@ int main(void)
     for more details.  */
     for (;;)
         ;
-}
-
-/*!
- * @brief Timer callback.
- */
-static void vExampleTimerCallback(TimerHandle_t xTimer)
-{
-    /* The timer has expired.  Count the number of times this happens.  The
-    timer that calls this function is an auto re-load timer, so it will
-    execute periodically. */
-    ulCountOfTimerCallbackExecutions++;
-}
-
-/*!
- * @brief Task prvQueueSendTask periodically sending message.
- */
-static void prvQueueSendTask(void *pvParameters)
-{
-    TickType_t xNextWakeTime;
-    const uint32_t ulValueToSend = 100UL;
-
-    /* Initialise xNextWakeTime - this only needs to be done once. */
-    xNextWakeTime = xTaskGetTickCount();
-
-    for (;;)
-    {
-        /* Place this task in the blocked state until it is time to run again.
-        The block time is specified in ticks, the constant used converts ticks
-        to ms.  While in the Blocked state this task will not consume any CPU
-        time. */
-        vTaskDelayUntil(&xNextWakeTime, mainQUEUE_SEND_PERIOD_MS);
-
-        /* Send to the queue - causing the queue receive task to unblock and
-        increment its counter.  0 is used as the block time so the sending
-        operation will not block - it shouldn't need to block as the queue
-        should always be empty at this point in the code. */
-        xQueueSend(xQueue, &ulValueToSend, 0);
-    }
-}
-
-/*!
- * @brief Task prvQueueReceiveTask waiting for message.
- */
-static void prvQueueReceiveTask(void *pvParameters)
-{
-    uint32_t ulReceivedValue;
-
-    for (;;)
-    {
-        /* Wait until something arrives in the queue - this task will block
-        indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-        FreeRTOSConfig.h. */
-        xQueueReceive(xQueue, &ulReceivedValue, portMAX_DELAY);
-
-        /*  To get here something must have been received from the queue, but
-        is it the expected value?  If it is, increment the counter. */
-        if (ulReceivedValue == 100UL)
-        {
-            /* Count the number of items that have been received correctly. */
-            ulCountOfItemsReceivedOnQueue++;
-            PRINTF("Receive message counter: %d.\r\n", ulCountOfItemsReceivedOnQueue);
-        }
-    }
-}
-
-/*!
- * @brief task prvEventSemaphoreTask is waiting for semaphore.
- */
-static void prvEventSemaphoreTask(void *pvParameters)
-{
-    for (;;)
-    {
-        /* Block until the semaphore is 'given'. */
-        if (xSemaphoreTake(xEventSemaphore, portMAX_DELAY) != pdTRUE)
-        {
-            PRINTF("Failed to take semaphore.\r\n");
-        }
-
-        /* Count the number of times the semaphore is received. */
-        ulCountOfReceivedSemaphores++;
-
-        PRINTF("Event task is running.\r\n");
-    }
-}
-
-/*!
- * @brief tick hook is executed every tick.
- */
-void vApplicationTickHook(void)
-{
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    static uint32_t ulCount             = 0;
-
-    /* The RTOS tick hook function is enabled by setting configUSE_TICK_HOOK to
-    1 in FreeRTOSConfig.h.
-
-    "Give" the semaphore on every 500th tick interrupt. */
-    ulCount++;
-    if (ulCount >= 500UL)
-    {
-        /* This function is called from an interrupt context (the RTOS tick
-        interrupt),    so only ISR safe API functions can be used (those that end
-        in "FromISR()".
-
-        xHigherPriorityTaskWoken was initialised to pdFALSE, and will be set to
-        pdTRUE by xSemaphoreGiveFromISR() if giving the semaphore unblocked a
-        task that has equal or higher priority than the interrupted task. */
-        xSemaphoreGiveFromISR(xEventSemaphore, &xHigherPriorityTaskWoken);
-        ulCount = 0UL;
-    }
-
-    /* If xHigherPriorityTaskWoken is pdTRUE then a context switch should
-    normally be performed before leaving the interrupt (because during the
-    execution of the interrupt a task of equal or higher priority than the
-    running task was unblocked).  The syntax required to context switch from
-    an interrupt is port dependent, so check the documentation of the port you
-    are using.
-
-    In this case, the function is running in the context of the tick interrupt,
-    which will automatically check for the higher priority task to run anyway,
-    so no further action is required. */
 }
 
 /*!
