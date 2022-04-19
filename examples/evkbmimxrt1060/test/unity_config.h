e628a3bc61d0ab88d1635c1fcb81031662dc71ae
Tianming Chen <tianmc@amazon.com>
2022-04-19 15:57:18 -0700

    Fixed formatting

--- examples/evkbmimxrt1060/test/unity_config.h
+++ examples/evkbmimxrt1060/test/unity_config.h
@@ -2,17 +2,16 @@
 #define UNITY_CONFIG_H
 
 #ifndef UNITY_OUTPUT_CHAR
-#error "creating custom output char"
-    #define UNITY_OUTPUT_CHAR( a )     TEST_CacheResult( a )
+#define UNITY_OUTPUT_CHAR( a )     TEST_CacheResult( a )
 #endif
 #ifndef UNITY_OUTPUT_FLUSH
-    #define UNITY_OUTPUT_FLUSH()       TEST_SubmitResultBuffer()
+#define UNITY_OUTPUT_FLUSH()       TEST_SubmitResultBuffer()
 #endif
 #ifndef UNITY_OUTPUT_START
-    #define UNITY_OUTPUT_START()       TEST_NotifyTestStart()
+#define UNITY_OUTPUT_START()       TEST_NotifyTestStart()
 #endif
 #ifndef UNITY_OUTPUT_COMPLETE
-    #define UNITY_OUTPUT_COMPLETE()    TEST_NotifyTestFinished()
+#define UNITY_OUTPUT_COMPLETE()    TEST_NotifyTestFinished()
 #endif
 
 #endif /* UNITY_CONFIG_H */
