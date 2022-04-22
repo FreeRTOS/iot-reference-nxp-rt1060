52b6c08facb1be88655ade9cab6404b7a1b36804
Tianming Chen <tianmc@amazon.com>
2022-04-21 18:01:39 -0700

    Added CLI mode in test project

--- examples/evkbmimxrt1060/test/app_main.c
+++ examples/evkbmimxrt1060/test/app_main.c
@@ -40,12 +40,17 @@
 
 #define appmainRUN_OTA_END_TO_END_TEST_SUITE      ( 0 )
 
+#define appmainPROVISIONING_MODE                  ( 0 )
+
 
 #define appmainMQTT_OTA_UPDATE_TASK_STACK_SIZE    ( 4096 )
 #define appmainMQTT_OTA_UPDATE_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1 )
 
 #define appmainTEST_TASK_STACK_SIZE               ( 6144 )
 #define appmainTEST_TASK_PRIORITY                 ( tskIDLE_PRIORITY + 1 )
+
+#define appmainCLI_TASK_STACK_SIZE                ( 6144 )
+#define appmainCLI_TASK_PRIORITY                  ( tskIDLE_PRIORITY + 1 )
 
 /**
  * @brief Stack size and priority for MQTT agent task.
@@ -61,11 +66,32 @@ extern void prvQualificationTestTask( void * pvParameters );
 
 extern void vSubscribePublishTestTask( void * pvParameters );
 
+extern void vCLITask( void * pvParam );
+
 int app_main( void )
 {
     BaseType_t xResult = pdFAIL;
 
     xResult = KVStore_init();
+    if( xResult == pdFAIL )
+    {
+        configPRINTF( ( "Failed to initialize key value configuration store.\r\n" ) );
+    }
+
+#if ( appmainPROVISIONING_MODE == 1 )
+    {
+        if( xResult == pdPASS )
+        {
+            xResult = xTaskCreate( vCLITask,
+                                   "CLI",
+                                   appmainCLI_TASK_STACK_SIZE,
+                                   NULL,
+                                   appmainCLI_TASK_PRIORITY,
+                                   NULL );
+        }
+    }
+#endif /* if ( appmainPROVISIONING_MODE == 1 ) */
+
 #if ( appmainRUN_QUALIFICATION_TEST_SUITE == 1 )
     {
         if( xResult == pdPASS )
@@ -100,6 +126,8 @@ int app_main( void )
         }
     }
 #endif /* if ( appmainRUN_DEVICE_ADVISOR_TEST_SUITE == 1 ) */
+
+
 
     return pdPASS;
 }
