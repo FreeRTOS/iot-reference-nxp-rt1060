87df8a08f8f43c495e95401f41ffd3ae6d78b759
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2023-11-14 20:24:30 -0800

    Remove test folder and lwip ethernet files

--- examples/evkbmimxrt1060/test/include/unity_config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UNITY_CONFIG_H
-#define _UNITY_CONFIG_H
-
-#define UNITY_OUTPUT_CHAR( a )     TEST_CacheResult( a )
-#define UNITY_OUTPUT_FLUSH()       TEST_SubmitResultBuffer()
-#define UNITY_OUTPUT_START()       TEST_NotifyTestStart()
-#define UNITY_OUTPUT_COMPLETE()    TEST_NotifyTestFinished()
-
-#endif
