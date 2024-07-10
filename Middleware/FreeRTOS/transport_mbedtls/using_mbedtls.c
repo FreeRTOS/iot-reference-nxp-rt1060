eb22b581afaab5b028726099e3571afb8b95d6af
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2024-07-10 11:49:25 -0700

    Use only a specified cipher suite (#50)
    * Use only a specified cipher suite
    
    * Clean up folder by removing support packages

--- Middleware/FreeRTOS/transport_mbedtls/using_mbedtls.c
+++ Middleware/FreeRTOS/transport_mbedtls/using_mbedtls.c
@@ -626,6 +626,13 @@ static TlsTransportStatus_t tlsSetup( NetworkContext_t * pNetworkContext,
         mbedtls_ssl_conf_cert_profile( &( pNetworkContext->sslContext.config ),
                                        &( pNetworkContext->sslContext.certProfile ) );
 
+        /* Only use this cipher suite to connect to the broker. */
+        const int ciphersuites[] = {
+               MBEDTLS_TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+               0 /* End of list. */
+           };
+        mbedtls_ssl_conf_ciphersuites( &( pNetworkContext->sslContext.config ), ciphersuites );
+
         /* Parse the server root CA certificate into the SSL context. */
         mbedtlsError = mbedtls_x509_crt_parse( &( pNetworkContext->sslContext.rootCa ),
                                                pNetworkCredentials->pRootCa,
